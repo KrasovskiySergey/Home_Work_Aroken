ecc86dd315d72ea1ec7b186fbf1ce5872f0ca85a
TheOneGuy <157389256+KrasovskiySergey@users.noreply.github.com>
2025-04-10 18:06:26 +0300

    update

--- Project/js/main.js
+++ Project/js/main.js
@@ -60,17 +60,17 @@
         const tabContentID = tabControl.getAttribute('href')
         const tabContent = document.querySelector(tabContentID)
         const activeControl = document.querySelector('.tab-controls__link--active')
-        const activeContent = document.querySelector('.tab-content--snow')
+        const activeContent = document.querySelector('.tab-content--show')
 
         if (activeControl) {
             activeControl.classList.remove('tab-controls__link--active')
         }
         if (activeContent) {
-            activeContent.classList.remove('tab-content--snow')
+            activeContent.classList.remove('tab-content--show')
         }
 
         tabControl.classList.add('tab-controls__link--active')
-        tabContent.classList.add('tab-content--snow')
+        tabContent.classList.add('tab-content--show')
         
     }
 

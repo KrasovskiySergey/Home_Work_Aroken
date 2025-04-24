0452cfc5f27d883d4585c625472047c7f5ee8d92
TheOneGuy <157389256+KrasovskiySergey@users.noreply.github.com>
2025-04-24 14:35:13 +0300

    update

--- Project/js/main.js
+++ Project/js/main.js
@@ -178,4 +178,11 @@
             }
         }
     });
+
+        //Маска для телефона
+
+        const telInputs = document.querySelectorAll('input[type="tel"]')
+    
+        const im = new Inputmask('+7(999) 999-99-99')
+        im.mask(telInputs)
 })()
